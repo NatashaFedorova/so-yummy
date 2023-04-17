01b66733cc12249f2bf1b06eacffb65b15e50a79
Наташа Федорова <98808643+NatashaFedorova@users.noreply.github.com>
2023-04-17 18:15:39 +0300

    fix

--- src/components/constants/Background/Background.styled.jsx
+++ src/components/constants/Background/Background.styled.jsx
@@ -4,6 +4,5 @@ const Box = styled.div`
   min-height: 100vh;
   color: ${props => props.theme.colors.text};
   background-color: ${props => props.theme.colors.background};
-  background: rgb(35, 38, 42);
 `;
 export default Box;
