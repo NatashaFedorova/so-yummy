35c59fb115531122c17a97e256b4723106944792
Olevova <olevova1983@gmail.com>
2023-04-18 20:56:44 +0300

    StartPageComponent dont change

--- src/components/StartPageComponent/StartPageComponent.jsx
+++ src/components/StartPageComponent/StartPageComponent.jsx
@@ -1,13 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import Container from 'components/constants/Container';
 import { Btn } from './StartPageComponent.styled';
-// import { CardRecipe } from '../CardRecipe/CardRecipe';
-
-// item for try card
-// const item = {
-//   title: 'pankcake',
-//   photo: `${process.env.PUBLIC_URL}/favicon/assort.jpg`,
-// };
 
 const StartPageComponent = () => {
   return (
@@ -16,7 +9,6 @@ const StartPageComponent = () => {
       <Btn>Registration</Btn>
       <NavLink to="register">Registration</NavLink>
       <NavLink to="signin">SignIn</NavLink>
-      {/* <CardRecipe item={item} /> */}
     </Container>
   );
 };
