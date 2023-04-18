dc1a79807c4e29e10c50514345da0f2c37179c1d
NatashaFedorova <98808643+NatashaFedorova@users.noreply.github.com>
2023-04-18 19:24:32 +0000

    Deploying to gh-pages from @ NatashaFedorova/so-yummy@7ee7bb08b3dc642e41f4f2715390839b12bc2163 🚀

--- static/js/239.0c01ab41.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkso_yummy=self.webpackChunkso_yummy||[]).push([[239],{599:function(u,n,t){t.r(n),t.d(n,{default:function(){return r}});var e=t(184),r=function(){return(0,e.jsx)("p",{children:"404"})}},239:function(u,n,t){t.r(n);var e=t(184),r=t(599).default;n.default=function(){return(0,e.jsx)(r,{})}}}]);
-//# sourceMappingURL=239.0c01ab41.chunk.js.map
