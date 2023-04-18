af9a91163ecc82a2c15469db533594e4c354e5e5
Наташа Федорова <98808643+NatashaFedorova@users.noreply.github.com>
2023-04-18 21:51:31 +0300

    fix

--- src/components/CardRecipe/CardRecipe.jsx
+++ src/components/CardRecipe/CardRecipe.jsx
@@ -4,10 +4,10 @@ import * as React from 'react';
 const CardRecipe = ({ item }) => {
   return (
     <RecipeCard>
-      <img src={photo} alt={n} />
+      {/* <img src={photo} alt={n} />
       <div class="overlay">
         <h3 class="title">{n}</h3>
-      </div>
+      </div> */}
     </RecipeCard>
   );
 };
