1d97aca0df700afff1368c4dc00057b60d573cb6
Наташа Федорова <98808643+NatashaFedorova@users.noreply.github.com>
2023-04-19 10:36:59 +0300

    fix container

--- src/components/constants/Container/Container.styled.jsx
+++ src/components/constants/Container/Container.styled.jsx
@@ -1,21 +1,21 @@
 import styled from 'styled-components';
-import { device } from '../deviceType/deviceType';
+import { device, size } from '../deviceType/deviceType';
 
 const Container = styled.div`
   margin: 0 auto;
 
   @media ${device.mobile} {
-    width: 375px;
+    width: ${size.mobile};
     padding: 0 16px;
   }
 
   @media ${device.tablet} {
-    width: 768px;
+    width: ${size.tablet};
     padding: 0 32px;
   }
 
   @media ${device.desktop} {
-    width: ${device.desktop};
+    width: ${size.desktop};
     padding: 0 100px;
   }
 `;
