1ee1634c9a3d6cf5ff7f0841c55fb9e3359c6a3c
Olevova <olevova1983@gmail.com>
2023-04-23 15:55:20 +0300

    add pagination component

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,5 +1,49 @@
-const Pagination = () => {
-  return <p>Pagination</p>;
-};
+import Pagination from 'rc-pagination';
+import * as React from 'react';
+import {
+  PaginationContainer,
+  PageButton,
+  Slider,
+  ContainerForAline,
+} from './Pagination.styled';
 
-export default Pagination;
+export const PagePagination = ({
+  cardsPerPage,
+  totalPages,
+  currentPage,
+  handlePageChange,
+}) => {
+  return (
+    <ContainerForAline>
+      <PaginationContainer>
+        <Pagination
+          total={totalPages}
+          pageSize={cardsPerPage}
+          current={currentPage}
+          onChange={handlePageChange}
+          prevIcon={<PageButton>{'<'}</PageButton>}
+          nextIcon={<PageButton>{'>'}</PageButton>}
+          itemRender={(page, type, element) => {
+            if (type === 'page') {
+              return (
+                <PageButton
+                  onClick={() => handlePageChange(page)}
+                  active={element.props.active}
+                >
+                  {page}
+                </PageButton>
+              );
+            }
+            if (type === 'prev') {
+              return element;
+            }
+            if (type === 'next') {
+              return element;
+            }
+            return <Slider />;
+          }}
+        />
+      </PaginationContainer>
+    </ContainerForAline>
+  );
+};
