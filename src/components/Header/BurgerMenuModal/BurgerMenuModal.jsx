4cf58976afe1fe60d4751b8d624dfbf741a9c3be
Наташа Федорова <98808643+NatashaFedorova@users.noreply.github.com>
2023-04-30 17:34:14 +0300

    fix

--- src/components/Header/BurgerMenuModal/BurgerMenuModal.jsx
+++ src/components/Header/BurgerMenuModal/BurgerMenuModal.jsx
@@ -11,7 +11,7 @@ import SwitchTheme from 'components/SwitchTheme';
 
 export const BurgerMenuModal = ({ closeBurgerMenuModal, showBurgerMenu }) => {
   return (
-    <BurgerModal style={showBurgerMenu ? { top: 0 } : { top: '-120%' }}>
+    <BurgerModal style={showBurgerMenu ? { right: 0 } : { right: '-120%' }}>
       <BurgerLogoDiv onClick={closeBurgerMenuModal}>
         <Logo />
       </BurgerLogoDiv>
